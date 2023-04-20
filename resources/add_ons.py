93ed1815cf973146222aa411585b7c82bc22ab06
kit4py <105520646+kit4py@users.noreply.github.com>
2023-04-20 21:24:33 +0200

    Delete resources directory

--- resources/add_ons.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime, date
-
-TODAY_DATE = str(date.today())
-NOW_TIME = datetime.now().strftime('%H-%M-%S')
-
-if __name__ == '__main__':
-    print(TODAY_DATE)
-    print(NOW_TIME)
